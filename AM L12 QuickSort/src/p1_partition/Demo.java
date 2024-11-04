ad38170d4bc828ddcf3994c448f1e92707cbe87e
B. Chen <chenb@sunysuffolk.edu>
2024-11-04 17:43:42 -0500

    Mergesort and Partitioning

---
+++ AM L12 QuickSort/src/p1_partition/Demo.java
@@ -0,0 +1,22 @@
+package p1_partition;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayPar arr = new ArrayPar(20);
+		arr.insert(3);
+		arr.insert(7);
+		arr.insert(9);
+		arr.insert(4);
+		arr.insert(2);
+		arr.insert(8);
+		arr.insert(1);
+		arr.insert(5);
+		arr.insert(6); 
+		arr.display();
+		int partition = arr.partitionIt(0, arr.size() - 1, 5);
+		arr.display();
+		System.out.println(partition);
+	}
+
+}
---
+++ PM L11 Quicksort/src/p1_parititioning/ArrayPar.java
@@ -0,0 +1,54 @@
+package p1_parititioning;
+
+public class ArrayPar {
+	private long[] arr;
+	private int nElems;
+	
+	public ArrayPar(int maxSize) {
+		arr = new long[maxSize];
+		nElems = 0;
+	}
+	
+	public int partitionIt(int left, int right, long pivot) {
+		int leftPtr = left - 1;
+		int rightPtr = right + 1;
+		
+		while(true) {
+			while(leftPtr < right && arr[++leftPtr] < pivot) {
+				; // no-op
+			}
+			
+			while(rightPtr > left && arr[--rightPtr] >= pivot) {
+				; // no-op
+			}
+			
+			if(leftPtr >= rightPtr) {
+				break;
+			} else {
+				swap(leftPtr, rightPtr);
+			}
+		}
+		return leftPtr;
+	}
+	
+	public void swap(int dex1, int dex2) {
+		long temp = arr[dex1];
+		arr[dex1] = arr[dex2];
+		arr[dex2] = temp;
+	}
+	
+	public int size() {
+		return nElems;
+	}
+	
+	public void insert(long value) {
+		arr[nElems++] = value;
+	}
+	
+	public void display() {
+		for(int i = 0; i < nElems; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+}
