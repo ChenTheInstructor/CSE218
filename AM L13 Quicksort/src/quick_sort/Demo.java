d718296a076007e54005b5bfa6c2d4fb903d0df2
B. Chen <chenb@sunysuffolk.edu>
2024-11-05 13:05:07 -0500

    quicksort

---
+++ AM L13 Quicksort/src/quick_sort/Demo.java
@@ -0,0 +1,20 @@
+package quick_sort;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		ArrayIns arr = new ArrayIns(20);
+		arr.insert(5);
+		arr.insert(8);
+		arr.insert(4);
+		arr.insert(7);
+		arr.insert(1);
+		arr.insert(2);
+		arr.insert(6);
+		arr.insert(3);
+		arr.display();
+		arr.quickSort();
+		arr.display();
+	}
+
+}
